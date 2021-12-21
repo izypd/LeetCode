fa664abc02f6e6625104109af84a06a688572c78
zy <52zypd@gmail.com>
2021-12-21 15:47:25 +0800

    142

---
+++ 101/03 双指针/03_142.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+  let fast = head;
+  let slow = head;
+  do {
+    if (!fast || !fast.next) return null;
+    fast = fast.next.next;
+    slow = slow.next;
+  } while (fast != slow);
+  fast = head;
+  while (fast != slow) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  return fast;
+};
